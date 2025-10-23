c1ee018512c47d8ccd4c8be7ae9787d49b5c1d92
PeterMonev <monevpeter@gmail.com>
2025-10-23 16:48:41 +0300

    solving task 2

---
+++ Lab - Encapsulation/02. Salary Increase/Person.cs
@@ -0,0 +1,63 @@
+﻿
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            private set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            private set { this.lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            private set { this.age = value; }
+        }
+
+        public decimal Salary
+        {
+
+            get { return this.salary; }
+            private set { this.salary = value; }
+
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.age} years old.";
+        }
+    }
+}
