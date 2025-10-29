1d533bb76416f9d3352e0255b0a8b9caa022e989
PeterMonev <monevpeter@gmail.com>
2025-10-29 12:04:18 +0200

    solving task 1

---
+++ Interfaces and Abstraction - Lab/01. Shapes/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public int Radius
+        {
+         
+            get { return radius; }
+            private set {  radius = value; }
+        }
+
+        public void Draw()
+        {
+            int r = Radius;
+            for (int y = -r; y <= r; y++)
+            {
+                for (int x = -r; x <= r; x++)
+                {
+                    if (x * x + y * y <= r * r)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Interfaces and Abstraction - Lab/01. Shapes/Rectangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+
+        private int width;
+        private int height;
+
+        public Rectangle (int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    if (i == 0 || i == Height - 1 || j == 0 || j == Width - 1)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
