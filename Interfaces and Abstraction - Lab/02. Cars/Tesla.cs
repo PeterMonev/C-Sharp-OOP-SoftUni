5cdc573a6033ca6615606056ddf512b05fe26865
PeterMonev <monevpeter@gmail.com>
2025-10-29 13:53:21 +0200

    solving task 2

---
+++ Interfaces and Abstraction - Lab/02. Cars/Tesla.cs
@@ -0,0 +1,38 @@
+﻿using Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar, ICar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries";
+        }
+
+    }
+}
