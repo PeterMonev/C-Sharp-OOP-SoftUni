3cf93e2499bae0cdf2a1ae80c525b804723c8df9
PeterMonev <monevpeter@gmail.com>
2025-10-30 11:00:02 +0200

    solving task 1

---
+++ Interfaces and Abstraction - Exercise/01. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } private set { name = value; } }
+
+        public int Age { get { return age; } private set { age = value; } }
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
