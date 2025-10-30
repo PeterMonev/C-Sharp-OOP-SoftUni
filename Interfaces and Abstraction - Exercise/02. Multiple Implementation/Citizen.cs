49324b3ab8810131dd8b83044383140d1cd524a9
PeterMonev <monevpeter@gmail.com>
2025-10-30 11:07:39 +0200

    solving task 2

---
+++ Interfaces and Abstraction - Exercise/02. Multiple Implementation/Citizen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string birthdate;
+
+        public Citizen(string name, int age, string id, string birthdate )
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthdate;
+        }
+        public string Name { get { return name; } private set { name = value; } }
+
+        public int Age { get { return age; } private set { age = value; } }
+
+        public string Id {  get { return id; } private set { id = value; } }
+
+        public string Birthdate { get { return birthdate; } private set { birthdate = value; } }
+    }
+}
