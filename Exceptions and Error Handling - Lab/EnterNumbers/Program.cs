7e9f97c352e172358098d70bb8d17cb93a4d8cf7
PeterMonev <monevpeter@gmail.com>
2025-11-06 14:06:40 +0200

    Solving task 2

---
+++ Exceptions and Error Handling - Lab/EnterNumbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnterNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int Start = 1;
+            const int End = 100;
+
+            List<int> numbers = new List<int>();
+            int currentStart = Start;
+
+            while (numbers.Count < 10)
+            {
+                try
+                {
+                    int number = ReadNumber(currentStart, End);
+                    numbers.Add(number);
+                    currentStart = number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int number))
+            {
+                throw new FormatException();
+            }
+
+            if (number <= start || number >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - {end}!");
+            }
+
+            return number;
+        }
+    }
+}
