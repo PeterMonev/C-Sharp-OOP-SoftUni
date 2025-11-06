8b2d83894414856782068256573a3d1155ecc6fe
PeterMonev <monevpeter@gmail.com>
2025-11-06 09:37:37 +0200

    Solving task 1

---
+++ Exceptions and Error Handling - Lab/Square Root/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Compression;
+
+namespace SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                if (n < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+
+                }
+                int result = (int)Math.Sqrt(n);
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
