747c2f70dc9b2448c53e4edcb3afd492d5aba005
PeterMonev <monevpeter@gmail.com>
2025-11-26 12:36:12 +0200

    Solving task 3

---
+++ Reflection and Attributes - Lab/MissionPrivateImpossible/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MissionPrivateImpossible
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var spy = new Spy();
+
+            var result = spy.RevealPrivateMethods("MissionPrivateImpossible.Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}
