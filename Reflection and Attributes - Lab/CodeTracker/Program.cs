396291c7885fd2b282e10ebf29ee4b9cd86952f5
PeterMonev <monevpeter@gmail.com>
2025-11-26 13:09:01 +0200

    Solving task 6

---
+++ Reflection and Attributes - Lab/CodeTracker/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeTracker
+{
+    [Author("Victor")]
+
+    public class Program
+    {
+        [Author("George")]
+
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/CodeTracker/Tracker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTracker
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            var allTypes = typeof(Tracker).Assembly.GetTypes();
+
+            foreach (var type in allTypes)
+            {
+                var methods = type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+                foreach (var method in methods)
+                {
+                var authorAtrribute = method.GetCustomAttributes<AuthorAttribute>().Where(attr => attr != null).Select(attr => attr.Name).ToList();
+
+                    if (authorAtrribute.Any())
+                    {
+                        Console.WriteLine($"{method.Name} is written by {string.Join(", " authorAtrribute)}");
+                    }
+
+                }
+            }
+        }
+    }
+}
