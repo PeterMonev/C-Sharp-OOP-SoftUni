e2d62cecc346f32ada739e1a05108ddc98970386
PeterMonev <monevpeter@gmail.com>
2025-11-26 16:42:32 +0200

    Solving task 1

---
+++ Reflection and Attributes - Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,30 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            string[] arguments = args.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = arguments[0];
+
+            string[] commandArguments = arguments.Skip(1).ToArray();
+
+            Type commandType = Assembly.GetEntryAssembly().GetTypes().FirstOrDefault(t => t.Name == $"{commandName}Command");
+
+            ICommand commandInstance = Activator.CreateInstance(commandType) as ICommand;
+
+            string result = commandInstance.Execute(commandArguments);
+
+            return result;
+        }
+    }
+}
