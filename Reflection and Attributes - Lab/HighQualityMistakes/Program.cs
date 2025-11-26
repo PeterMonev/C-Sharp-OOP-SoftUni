d59cbfc2083ca2acee187902827f27c614bb1fca
PeterMonev <monevpeter@gmail.com>
2025-11-26 12:18:18 +0200

    Solving task 2

---
+++ Reflection and Attributes - Lab/HighQualityMistakes/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HighQualityMistakes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var spy = new Spy();
+
+            var result = spy.AnalyzeAccessModifiers("HighQualityMistakes.Hacker");
+
+            
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/HighQualityMistakes/Spy.cs
@@ -0,0 +1,70 @@
+﻿
+using System.Reflection;
+using System.Text;
+
+namespace HighQualityMistakes
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldNames)
+        {
+
+            Console.WriteLine($"Class under investigation: {className}");
+
+            var type = Type.GetType(className);
+
+            var fields = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            var searchedFields = fields.Where(f => fieldNames.Contains(f.Name)).ToList();
+
+            var instance = Activator.CreateInstance(type);
+
+            var result = new StringBuilder();
+
+            foreach (var field in searchedFields)
+            {
+                var value = field.GetValue(instance);
+
+                result.AppendLine($"{field.Name} = {value}");
+            }
+
+            return result.ToString();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            var type = Type.GetType(className);
+
+            var result = new StringBuilder();
+
+            var fields = type.GetFields();
+
+            foreach (var field in fields)
+            {
+                result.AppendLine($"{field.Name} must be private!");
+            }
+
+            var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            foreach (var property in properties)
+            {
+                var getMethod = property.GetGetMethod(true);
+
+                if (getMethod != null && !getMethod.IsPublic)
+                {
+                    result.AppendLine($"{getMethod.Name} have to be public!");
+                }
+
+                var setMethod = property.GetSetMethod();
+
+                if (setMethod != null && !setMethod.IsPrivate)
+                {
+                    result.AppendLine($"{setMethod.Name} have to be private!");
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
+
