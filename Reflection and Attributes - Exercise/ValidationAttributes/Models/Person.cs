2ac95762aa1a583b6e46650cd9b738fb76bdac00
PeterMonev <monevpeter@gmail.com>
2025-11-27 14:11:59 +0200

    Solving task 2

---
+++ Reflection and Attributes - Exercise/ValidationAttributes/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Models
+{
+    public class Person
+    {
+        private string fullName;
+        private int age;
+
+        public Person(string fullName, int age)
+        {
+            FullName = fullName;
+            Age = age;
+     
+        }
+
+        public string FullName { get; private set; }
+
+        [MyRange(12, 90)]
+        public int Age {  get; private set; }
+    }
+}
---
+++ Reflection and Attributes - Exercise/ValidationAttributes/Utils/Validator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utils
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type type = obj.GetType();
+
+            PropertyInfo[] properties = type.GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                IEnumerable<MyValidationAttribute> customValidationAttributes = property.GetCustomAttributes<MyValidationAttribute>();
+
+                foreach (MyValidationAttribute attribute in customValidationAttributes)
+                {
+                    object value = property.GetValue(obj);
+
+                    if (!attribute.IsValid(value))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
