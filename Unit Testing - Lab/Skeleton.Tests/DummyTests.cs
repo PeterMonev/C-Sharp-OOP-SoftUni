d2f0fb02facc789083d0e7972f7f266b303e161a
PeterMonev <monevpeter@gmail.com>
2025-11-28 10:43:59 +0200

    Solving task 2

--- Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
+++ Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,9 +7,47 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void DummuLosesHealthIfAttacked()
         {
+            var axe = new Axe(10, 20);
+            var dummy = new Dummy(100, 100);
 
+            axe.Attack(dummy);
+
+            Assert.That(dummy.Health, Is.EqualTo(90));
+        }
+
+        [Test]
+
+        public void DeadDummyThrowsAnExceptionIfAttacked()
+        {
+            var axe = new Axe(10, 20);
+            var dummy = new Dummy(0, 100);
+
+            Assert.Throws<InvalidOperationException>(() => { axe.Attack(dummy); }, "Dummy is dead.");
+        }
+
+        [Test]
+
+        public void DeadDummyCanGiveXP()
+        {
+            var dummy = new Dummy(0, 100);
+
+            var result = dummy.GiveExperience();
+
+            Assert.That(result, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void AliveDummyCantGiveXP()
+        {
+            var dummy = new Dummy(100, 100);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var result = dummy.GiveExperience();
+
+            }, "Target is not dead.");
         }
     }
 }
